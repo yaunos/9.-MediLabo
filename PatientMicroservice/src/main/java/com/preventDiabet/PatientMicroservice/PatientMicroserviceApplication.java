5d6e95f3b26eaffe94806391454a38454529c0d6
DevPC <a.roche.c@gmail.com>
2023-12-07 23:17:34 +0100

    PatientMicroService Application was missing because of an error in the unzipping of the zip created from start.spring.io

---
+++ PatientMicroservice/src/main/java/com/preventDiabet/PatientMicroservice/PatientMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.preventDiabet.PatientMicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatientMicroserviceApplication.class, args);
+	}
+
+}
