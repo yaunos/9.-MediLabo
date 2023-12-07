362a5bca64f9f0801b1bc40ddbffe556d3a53797
DevPC <a.roche.c@gmail.com>
2023-12-07 23:21:16 +0100

    PatientMicroServiceApplication Tests was missing because of an error in the unzipping of the zip created from start.spring.io

---
+++ PatientMicroservice/src/test/java/PatientMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.preventDiabet.PatientMicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
