507a541cdb94530177d92f3027ffadb4442fcb9b
DevPC <a.roche.c@gmail.com>
2024-02-20 00:22:44 +0100

    docker-compose.yml running from Command docker-compose Changing path of MySQL scripts for debugging -> Still an error on empty medilabo database

--- infrastructure/mysql/1-patient.sql
+++ infrastructure/mysql/1-patient.sql
@@ -14,6 +14,6 @@ CREATE TABLE patient
 
 
 INSERT INTO `patient` (`firstname`, `lastname`, `date_of_birth`, `gender`, `address`, `phone_number`) VALUES ('Test', 'TestNone', '1966-12-31', 'F', '1 Brookside St','100-222-3333');
-INSERT INTO `patient` (`firstname`, `lastname`, `date_of_birth`, `gender`, `address`, `phone_number`) VALUES ('Test', 'TestBorderline', '1945-06-24', 'F', '2 High St','200-333-4444');
-INSERT INTO `patient` (`firstname`, `lastname`, `date_of_birth`, `gender`, `address`, `phone_number`) VALUES ('Test', 'TestInDanger', '2004-06-18', 'F', '3 Club Road','300-444-5555');
+INSERT INTO `patient` (`firstname`, `lastname`, `date_of_birth`, `gender`, `address`, `phone_number`) VALUES ('Test', 'TestBorderline', '1945-06-24', 'M', '2 High St','200-333-4444');
+INSERT INTO `patient` (`firstname`, `lastname`, `date_of_birth`, `gender`, `address`, `phone_number`) VALUES ('Test', 'TestInDanger', '2004-06-18', 'M', '3 Club Road','300-444-5555');
 INSERT INTO `patient` (`firstname`, `lastname`, `date_of_birth`, `gender`, `address`, `phone_number`) VALUES ('Test', 'TestEarlyOnSet', '2002-06-28', 'F', '4 Valley Dr','400-555-6666');
